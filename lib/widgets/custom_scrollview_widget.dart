e4aec9094160c7176ad4003c85aacaf3041e5659
Yunus Ayırgül <yunusayirgul@hotmail.com>
2023-07-28 11:46:08 +0300

    custom sliver

--- lib/widgets/custom_scrollview_widget.dart
+++ lib/widgets/custom_scrollview_widget.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:flutter/material.dart';
+import 'dart:math' as obj_color;
 
 class CustomScrollViewWidget extends StatelessWidget {
   const CustomScrollViewWidget({super.key});
@@ -56,8 +59,128 @@ class CustomScrollViewWidget extends StatelessWidget {
               fit: BoxFit.cover,
             ),
           ),
+        ),
+        // T : Sabit elemanlarla bir satırda kaç eleman olacağını söyleyeceğimiz GRID türü
+        SliverGrid(
+            delegate: SliverChildListDelegate(static_list()),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2)),
+        // Dinamik (builder ile üretilen) elemanlarla bir satırda kaç eleman olacağını söyleyeceğimiz GRID türü
+        SliverGrid(
+            delegate: SliverChildBuilderDelegate(dynamic_list, childCount: 6),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3)),
+        // Dinamik (builder ile üretilen) elemanlarla bir satırdaki  bir elemanın max genişliğini söylediğimiz GRID türü
+        SliverGrid(
+            delegate: SliverChildBuilderDelegate(dynamic_list, childCount: 6),
+            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 100)),
+        SliverGrid.count(
+          crossAxisCount: 3,
+          children: static_list(),
+        ),
+        SliverGrid.extent(
+          maxCrossAxisExtent: 300,
+          children: static_list(),
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.all(5),
+          sliver: SliverList(delegate: SliverChildListDelegate(static_list())),
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.all(20),
+          sliver: SliverFixedExtentList(
+            delegate: SliverChildBuilderDelegate(dynamic_list, childCount: 8),
+            itemExtent: 50,
+          ),
         )
       ],
     );
+  }
+
+  Color color_random() {
+    return Color.fromARGB(
+        obj_color.Random().nextInt(255),
+        obj_color.Random().nextInt(255),
+        obj_color.Random().nextInt(255),
+        obj_color.Random().nextInt(255));
+  }
+
+  Widget dynamic_list(BuildContext context, int index) {
+    return Container(
+      color: color_random(),
+      height: 100,
+      alignment: Alignment.center,
+      child: Text(
+        "D - ${index + 1}",
+        style: const TextStyle(fontSize: 18),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+
+  List<Widget> static_list() {
+    return [
+      Container(
+        height: 100,
+        color: Colors.amber,
+        alignment: Alignment.center,
+        child: const Text(
+          'Statik - 1',
+          style: TextStyle(fontSize: 18),
+          textAlign: TextAlign.center,
+        ),
+      ),
+      Container(
+        height: 100,
+        color: Colors.teal,
+        alignment: Alignment.center,
+        child: const Text(
+          'Statik - 2',
+          style: TextStyle(fontSize: 18),
+          textAlign: TextAlign.center,
+        ),
+      ),
+      Container(
+        height: 100,
+        color: Colors.blue,
+        alignment: Alignment.center,
+        child: const Text(
+          'Statik - 3',
+          style: TextStyle(fontSize: 18),
+          textAlign: TextAlign.center,
+        ),
+      ),
+      Container(
+        height: 100,
+        color: Colors.orange,
+        alignment: Alignment.center,
+        child: const Text(
+          'Statik - 4',
+          style: TextStyle(fontSize: 18),
+          textAlign: TextAlign.center,
+        ),
+      ),
+      Container(
+        height: 100,
+        color: Colors.purple,
+        alignment: Alignment.center,
+        child: const Text(
+          'Statik - 5',
+          style: TextStyle(fontSize: 18),
+          textAlign: TextAlign.center,
+        ),
+      ),
+      Container(
+        height: 100,
+        color: Colors.cyan,
+        alignment: Alignment.center,
+        child: const Text(
+          'Statik - 6',
+          style: TextStyle(fontSize: 18),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    ];
   }
 }
